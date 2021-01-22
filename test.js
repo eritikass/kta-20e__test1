1cec2a60afdd965adb21bec064f4685406cdac46
Siim Tiilen <eritikass@gmail.com>
2021-01-22 09:23:17 +0200

    init add of example

---
+++ test.js
@@ -0,0 +1,13 @@
+const assert = require("assert").strict;
+
+function sum(a, b) {
+    return a + b;
+}
+
+
+assert(sum(1,2) === 3, "1+2=3");
+
+assert.equal(sum(1,2), 3);
+assert.notEqual(sum(1,2), "3");
+
+assert.notEqual(sum(1,2), 1);
